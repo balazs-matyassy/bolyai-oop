a49dbe483e79ee41948e855dc1bca657bd56cb7c
Balázs Mátyássy <balazs.matyassy@hotmail.com>
2022-09-27 14:11:45 +0200

    balazs.matyassy: programozási tételek

--- src/bolyai/Main.java
+++ src/bolyai/Main.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class Main {
 
@@ -30,7 +32,69 @@ public class Main {
             // System.out.print: NEM formázott, NINCS sortörés
             // System.out.println: NEM formázott, VAN sortörés
             // System.out.printf: formázott, NINCS sortörés
+            // (4. feladat)
             System.out.printf("4. feladat: Döntők száma: %d\n", dontok.size());
+
+            // (5. feladat)
+            int ossz_kulonbseg = 0;
+
+            // for-each (összegzés)
+            for (Donto donto : dontok) {
+                // adott meccshez tartozó különbség kiszámítása
+                String[] eredmenyek = donto.eredmeny.split("-");
+                int gyoztesEredmeny = Integer.parseInt(eredmenyek[0]);
+                int vesztesEredmeny = Integer.parseInt(eredmenyek[1]);
+                // Math.abs nem feltétlenül szükséges, mivel gyoztesEredmeny >= vesztesEredmeny
+                int kulonbseg = Math.abs(gyoztesEredmeny - vesztesEredmeny);
+
+                ossz_kulonbseg += kulonbseg;
+            }
+
+            // double: tört szám
+            // ossz_kulonbseg / dontok.size(): egész számok osztása (elveszik a tört)
+            // double tort = 5 / 3; // ? -> tort = 1; (tört elveszik, mivel 2 egészet osztunk)
+            // (double): kasztolás (törtté alakítás) -> NEM jelentkezik a probléma
+            double atlag_kulonbseg = (double) ossz_kulonbseg / dontok.size();
+            System.out.printf("5. feladat: Átlagos pontkülönbség: %.1f\n", atlag_kulonbseg);
+
+            // 6. feladat (maximum keresés - OOP verzió)
+            // nem számot, hanem teljes sort / rekordot / objektumot... keresünk
+            Donto maxNezoszam = null; // null: érvénytelen érték (nincs adat)
+
+            for (Donto donto : dontok) {
+                // első iterációnál mindenképpen felülírásra kerül (null miatt)
+                // || VAGY (short circuit / rövidzáras kiértékelés)
+                // HA döntő == null, akkor a  donto.nezoszam > maxNezoszam.nezoszam összehasonlítás
+                // NEM kerül kiértékelésre.
+                // donto.nezoszam >= null.nezoszam -> NullPointerException kivétel keletkezik.
+                if (maxNezoszam == null || donto.nezoszam > maxNezoszam.nezoszam) {
+                    maxNezoszam = donto;
+                }
+            }
+
+            RomaiSorszam romaiSorszam = new RomaiSorszam(maxNezoszam.sorszam);
+            String arabSorszam = romaiSorszam.getArabSsz();
+
+            // redundáns, korábban már elvégeztük
+            String[] eredmenyek = maxNezoszam.eredmeny.split("-");
+            int gyoztesEredmeny = Integer.parseInt(eredmenyek[0]);
+            int vesztesEredmeny = Integer.parseInt(eredmenyek[1]);
+
+            System.out.println("6. feladat: Legmagasabb nézőszám a döntők során:");
+            System.out.printf("\tSorszám (dátum): %s (%s)\n", arabSorszam, maxNezoszam.datum);
+            System.out.printf(
+                    "\tGyőztes csapat: %s, szerzett pontok: %d\n",
+                    maxNezoszam.gyoztes,
+                    gyoztesEredmeny
+            );
+            System.out.printf(
+                    "\tVesztes csapat: %s, szerzett pontok: %d\n",
+                    maxNezoszam.vesztes,
+                    vesztesEredmeny
+            );
+            System.out.printf("\tHelyszín: %s\n", maxNezoszam.helyszin);
+            System.out.printf("\tVáros, állam: %s\n", maxNezoszam.varosAllam);
+            System.out.printf("\tNézőszám: %d\n", maxNezoszam.nezoszam);
 
             /*
                 3. Adatok kiírása
@@ -105,18 +169,28 @@ public class Main {
     private static void dontokKiirasa(String utvonal, List<Donto> dontok) throws IOException {
         // Írásra nyitunk meg egy fájlt.
         // Itt sincs hibakezelés (ebben az esetben a külső catch kezeli az esetleges hibákat).
-        try (PrintWriter writer = new PrintWriter(utvonal)) {
+        // (7. feladat)
+        try (PrintWriter writer = new PrintWriter("SuperBowlNew.txt")) {
+            // Létezik HasMap<T> is. Kicsit gyorsabb, de nem rendezett.
+            Map<String, Integer> reszvetelekSzama = new TreeMap<>();
+
             // Ha szeretnénk fejlécet:
-            writer.println("Ssz;Dátum;Győztes;Eredmény;Vesztes;Helyszín;VárosÁllam;Nézőszám");
+            writer.println("Ssz;Dátum;Győztes;Eredmény;Vesztes;Nézőszám");
 
             // for-each szerkezet
             // Egy gyűjtemény (List<T>, Set<T> vagy T[] (tömb)) minden elemét értékenként bejárunk.
             // A feldolgozáshoz nincs szükség az indexre.
             for (Donto donto : dontok) {
+                // régi érték + 1 vagy 1 (ha még nem volt régi érték)
+                int gyoztesSzereplesek = reszvetelekSzama.getOrDefault(donto.gyoztes, 0) + 1;
+                reszvetelekSzama.put(donto.gyoztes, gyoztesSzereplesek); // számláló frissítése
+                int vesztesSzereplesek = reszvetelekSzama.getOrDefault(donto.vesztes, 0) + 1;
+                reszvetelekSzama.put(donto.vesztes, vesztesSzereplesek);
+
                 // Döntő visszaalakítása sorrá a segédmetódus segítségével.
                 // Ebben az esetben változatlan formában írjuk ki az értékeket,
                 // de amúgy lehtőség lenne a dontobolSor metódusban egyéb formátumra is konvertálni.
-                String sor = dontobolSor(donto);
+                String sor = dontobolSor(donto, gyoztesSzereplesek, vesztesSzereplesek);
                 // Konvertált sor kiírása a megnyitott fájlba.
                 // lsd. System.out.println: NINCS formázás, VAN sortörés.
                 writer.println(sor);
@@ -124,14 +198,19 @@ public class Main {
         }
     }
 
-    private static String dontobolSor(Donto donto) {
-        return donto.sorszam
+    private static String dontobolSor(
+            Donto donto,
+            int gyoztesSzereplesek,
+            int vesztesSzereplesek
+    ) {
+        RomaiSorszam romaiSorszam = new RomaiSorszam(donto.sorszam);
+        String arabSorszam = romaiSorszam.getArabSsz();
+
+        return arabSorszam
                 + ";" + donto.datum
-                + ";" + donto.gyoztes
+                + ";" + donto.gyoztes + " (" + gyoztesSzereplesek + ")"
                 + ";" + donto.eredmeny
-                + ";" + donto.vesztes
-                + ";" + donto.helyszin
-                + ";" + donto.varosAllam
+                + ";" + donto.vesztes + " (" + vesztesSzereplesek + ")"
                 + ";" + donto.nezoszam; // számból automatikus a szöveggé konvertálás
     }
 
